b567311ff09b4ca90c4f4aeb4bd4b43e48fde4e5
rafinha <rafinha72465@gmail.com>
2026-03-31 14:42:49 -0300

    bla

--- fake_api/lib/src/models/travel.dart
+++ fake_api/lib/src/models/travel.dart
@@ -1,17 +1,17 @@
-
 /* this entity represents the Travel itself.. */
 import 'package:mock_repository/mock_repository.dart';
-class Travel{
+
+class Travel {
   final String travelName;
   final RoutePlan routePlan;
   final Itinerary itinerary;
 
   /// from jsonMethod
-  factory Travel.fromJson(Map<String, dynamic> json){
+  factory Travel.fromJson(Map<String, dynamic> json) {
     return Travel(
-        travelName: json['travelName']
-        routePlan: json['routePlan'],
-        itinerary: json['itinerary']
+      travelName: json['travelName'],
+      routePlan: json['routePlan'],
+      itinerary: json['itinerary'],
     );
   }
 
@@ -19,16 +19,12 @@ class Travel{
   ///
 
   Map<String, dynamic> toMap() {
-    return {
-      'routePlan': routePlan.toMap(),
-      'itinerary': itinerary.toMap(),
-    };
+    return {'routePlan': routePlan.toMap(), 'itinerary': itinerary.toMap()};
   }
 
-
-
-
-
-  Travel({required this.travelName, required this.routePlan, required this.itinerary});
-
+  Travel({
+    required this.travelName,
+    required this.routePlan,
+    required this.itinerary,
+  });
 }
