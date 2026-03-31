a11f42f392d86c204ef2ca41ead3abc3f1bd59cc
Luiz Lenzi <lu1zgustavolenz1@gmail.com>
2026-03-31 15:37:10 -0300

    fix: removendo submodulo a forca e subindo a pasta real da api

---
+++ compass-api/src/main/java/com/compass/compass_system/CompassSystemApplication.java
@@ -0,0 +1,13 @@
+package com.compass.compass_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompassSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompassSystemApplication.class, args);
+	}
+
+}
