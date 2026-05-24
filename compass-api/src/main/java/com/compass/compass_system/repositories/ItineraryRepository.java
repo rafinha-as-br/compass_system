974f7be5c04a9d5f8481d2b22db010010749a4bd
Luiz Lenzi <lu1zgustavolenz1@gmail.com>
2026-05-24 14:06:14 -0300

    clean: remove legacy duplicate files and incomplete travel controller/entities

--- compass-api/src/main/java/com/compass/compass_system/repositories/ItineraryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.compass.compass_system.repositories;
-
-import com.compass.compass_system.entities.Itinerary;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItineraryRepository extends JpaRepository<Itinerary, String> {
-}
--- compass-api/src/main/java/com/compass/compass_system/repositories/RoutePlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.compass.compass_system.repositories;
-
-import com.compass.compass_system.entities.RoutePlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoutePlanRepository extends JpaRepository<RoutePlan, Long> {
-}
